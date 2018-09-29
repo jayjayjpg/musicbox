3e3636bcde06837f288ab2b9c80c1b3c048272f4
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2018-09-29 22:13:41 +0200

    add sample and first seq

--- app/utils/newyork-feels.js
+++ app/utils/newyork-feels.js
@@ -3,54 +3,70 @@
 export default function newyorkFeels() {
   //create a synth and connect it to the master output (your speakers)
 
-  /*var bell = new Tone.NoiseSynth({
-    "harmonicity" : 12,
-    "resonance" : 800,
-    "modulationIndex" : 20,
-    "envelope" : {
-      "decay" : 0.4,
-    },
-    "volume" : -20
-  }).toMaster(); */
-  /*var bellPart = new Tone.Sequence(function(time, freq){
-    // bell.frequency.setValueAtTime(freq, time, Math.random()*0.5 + 0.5);
-    bell.triggerAttack(time);
-  }, [300, null, 200, null, 200, 200, null, 200, null, 200, null, 200], "4t").start(0);
-  // bellPart.loop = true;
-  // bellPart.loopEnd = "1m"; */
-  /* var conga = new Tone.MembraneSynth({
-    "pitchDecay" : 0.1,
-    "octaves" : 2,
-    "envelope" : {
+  var conga = new Tone.PluckSynth({
+    // "pitchDecay" : 0.1,
+    'dampening': 2000,
+    'resonance': 0.8,
+    /* "envelope" : {
       "attack" : 0.0006,
       "decay" : 0.5,
       "sustain" : 0
-    },
-    "volume": -5,
+    }, */
+    "volume": 5,
   }).toMaster();
+
+let motifOneA =  [null, "A4", null, "A4", "A4", "D5", "A4", "D5", "F5", null, null, null, null, null, null, null];
+let motifOneB = [null, "A4", null, "A4", "A4", "D5", "A4", "D5", "F5", "C6", "A6", "A6", "C6", "F5", "D5", "A4"];
   var congaPart = new Tone.Sequence(function(time, pitch){
     conga.triggerAttack(pitch, time, Math.random()* 0.7 + 0.3);
-  }, ["G2", "C3", "A2", "D2"], "4n").start(0); */
+  }, [motifOneA, motifOneA, motifOneA, motifOneB], "1n");
 
   var player = new Tone.Player({
-    "url" : "assets/audio/metroloop.mp3",
-    "loop" : false,
-    "loopStart": '1s',
-    "loopEnd": '10s',
+    "url" : "assets/audio/metrointro.mp3",
+    "loop" : true,
+    'loopStart': '16s',
+    "fadeIn": '5s',
+    "fadeOut": '2s',
+    "volume": -7,
   }).toMaster();
-  player.autostart = true;
+  // player.autostart = true;
   // intro.setLoopPoints('10s', '12s');
 
-  /* var player = new Tone.Player("assets/audio/metroloop.mp3").toMaster();
-  //play as soon as the buffer is loaded
-  player.loop = true;
-  player.setLoopPoints(0.6, 0.9);
-  var congaPart = new Tone.Sequence(function(time, pitch){
-    player.start();
-  //  player.triggerAttack(pitch, time, Math.random()* 0.7 + 0.3);
-}, ["G2", "C3", "A2", "D2"], "1n").start('5m'); */
-  /* congaPart.loop = true;
-  congaPart.loopEnd = "1m"; */
+  var player2 = new Tone.Player({
+    'url': "assets/audio/metroloop.mp3",
+    'loop': 'true',
+    'loopStart': '0.25',
+    'loopEnd': '0.75',
+    'fadeIn': '0.3s',
+    'volume': -7,
+  }).toMaster();
+
   Tone.Transport.bpm.value = 120;
+  Tone.Buffer.on('load', function(){
+    sampleTrack(player);
+  });
+
+  Tone.Transport.schedule(function() {
+    player.start();
+  }, '0');
+
+  Tone.Transport.schedule(function() {
+    player2.start();
+    player.stop();
+  }, '21');
+
+  Tone.Transport.schedule(function(time) {
+    // congaPart.start('0s');
+    congaPart.start('25');
+  });
+
+  /* Tone.Transport.schedule(function(time) {
+    congaPart2.start('8s');
+    // congaPart.start('25s');
+  }); */
+}
+
+function sampleTrack(sample) {
+  //sample.start();
   Tone.Transport.start();
 }
